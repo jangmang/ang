fd8c1c2a877ae0e229ea85c7a2a255158eb792c4
jjmh <zzzmh@naver.com>
2019-03-19 19:12:17 +0900

    삭제기능추가

--- ng-app2/src/app/todo/todos/todos.component.ts
+++ ng-app2/src/app/todo/todos/todos.component.ts
@@ -16,7 +16,9 @@ export class TodosComponent implements OnInit {
         this.todos = [
             { id:1, text:"운동하기" },
             { id:2, text:"공부하기" },
-            { id:3, text:"공부하기" },
+            { id:3, text:"강아지 산책시키기" },
+            { id:4, text:"동창회 모임" },
+            { id:5, text:"학원가기" },
         ];
     }
 
@@ -28,6 +30,17 @@ export class TodosComponent implements OnInit {
             id: this.todos.length + 1,
             text: text
         });
+    }
+
+    clearTodo(todo){
+        let idx = this.todos.findIndex(function(item){
+            return item.id === todo.id;
+        });
+        this.todos.splice(idx,1); 
+    }
+
+    clear(){
+        this.todos = [];
     }
   
 
